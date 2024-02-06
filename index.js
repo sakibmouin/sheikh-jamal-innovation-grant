d2be90372bd126b7067b1d30b2202c2cc2795234
Nazmus Sakib Mouin <71378784+sakibmouin@users.noreply.github.com>
2024-02-06 08:31:57 +0600

    add sticky navbar

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Sticky Navbar
 window.addEventListener("scroll", function () {
   var header = document.getElementById("header");
-  var sticky = header.offsetTop;
-
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+  header.classList.toggle("sticky", this.window.scrollY > 0);
 });
 
 // navbar dropdown
