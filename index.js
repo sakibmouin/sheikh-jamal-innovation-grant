72caf75df5ac6d22febc8163aa75e8a4c0f31a60
Nazmus Sakib Mouin <nsmouin7@gmail.com>
2024-03-17 16:28:00 +0600

    update

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 // Sticky Navbar
 window.addEventListener("scroll", function () {
-  var header = document.getElementById("header");
-  header.classList.toggle("sticky", this.window.scrollY > 0);
+	var header = document.getElementById("header");
+	header.classList.toggle("sticky", this.window.scrollY > 0);
 });
 
 // navbar dropdown
 document.addEventListener("DOMContentLoaded", function () {
-  const dropdownToggle = document.getElementById("dropdownNavbarLink");
-  const dropdownMenu = document.getElementById("dropdownNavbar");
+	const dropdownToggle = document.getElementById("dropdownNavbarLink");
+	const dropdownMenu = document.getElementById("dropdownNavbar");
 
-  dropdownToggle.addEventListener("click", function () {
-    dropdownMenu.classList.toggle("hidden");
-  });
+	dropdownToggle.addEventListener("click", function () {
+		dropdownMenu.classList.toggle("hidden");
+	});
 });
 document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menuToggle");
-  const navbarDropdown = document.getElementById("navbar-dropdown");
+	const menuToggle = document.getElementById("menuToggle");
+	const navbarDropdown = document.getElementById("navbar-dropdown");
 
-  menuToggle.addEventListener("click", function () {
-    // Toggle the 'hidden' class on the menu
-    navbarDropdown.classList.toggle("hidden");
-  });
+	menuToggle.addEventListener("click", function () {
+		// Toggle the 'hidden' class on the menu
+		navbarDropdown.classList.toggle("hidden");
+	});
 });
 
 // Cursor
 function loadCursorScript() {
-  var script = $("<script>").attr("src", "./cursor.js");
-  $("body").append(script);
+	var script = $("<script>").attr("src", "./cursor.js");
+	$("body").append(script);
 }
 function checkScreenSizeAndLoadScript() {
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    loadCursorScript();
-  } else {
-    $('script[src="./cursor.js"]').remove();
-  }
+	if (window.matchMedia("(min-width: 768px)").matches) {
+		loadCursorScript();
+	} else {
+		$('script[src="./cursor.js"]').remove();
+	}
 }
 checkScreenSizeAndLoadScript();
 $(window).resize(checkScreenSizeAndLoadScript);
 
 // Right click disabler
 $("img").on("contextmenu", function (e) {
-  e.preventDefault();
+	e.preventDefault();
 });
-
-// Typing Effect
